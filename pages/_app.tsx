cccfbba7f1593475ee6b1b0c93974798df4372d5
Solomon Hawk <solomon.hawk@viget.com>
2022-07-01 02:22:24 -0400

    add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,14 @@ import { loggerLink } from '@trpc/client/links/loggerLink';
 import type { AppProps } from 'next/app';
 import { withTRPC } from '@trpc/next';
 import { AppRouter } from 'server/router';
-import superjson from 'superjson';
 import { AppLayout } from 'components/layouts/app-layout';
+import superjson from 'superjson';
+import nProgress from 'nprogress';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

461def1f731f35e383a815b685a87fafa38aea29
Solomon Hawk <solomon.hawk@viget.com>
2022-07-05 04:38:15 -0400

    fix bug with /item redirect in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 import qs from 'query-string';
 
 export function middleware(request: NextRequest) {
-  // guard /item against missing `id` param
+  // guard /item?id=:id against missing `id` param
   if (request.nextUrl.pathname.startsWith('/item')) {
     if (!qs.parse(request.nextUrl.search)?.id) {
-      return NextResponse.redirect(new URL('/'));
+      return NextResponse.redirect(new URL('/', request.url));
     }
   }
 

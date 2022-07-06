e7eb608675218fa9eb4dc5b765c79ff0ee36ac87
Solomon Hawk <solomon.hawk@viget.com>
2022-07-06 02:55:05 -0400

    enable trpc api request caching for logged out users

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -2,6 +2,8 @@ import * as trpcNext from '@trpc/server/adapters/next';
 import { createContext } from 'server/context';
 import { appRouter } from 'server/router';
 
+const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   batching: {
@@ -13,5 +15,18 @@ export default trpcNext.createNextApiHandler({
       // send to bug reporting
       console.error('Something went wrong', error);
     }
+  },
+  responseMeta({ ctx, paths, type, errors }) {
+    if (ctx?.res && !ctx?.session?.user && errors.length === 0 && type === 'query') {
+      console.log('Returning cached query', paths);
+
+      return {
+        headers: {
+          'cache-control': `s-maxage=1, stale-while-revalidate=${ONE_DAY_IN_SECONDS}`,
+        },
+      };
+    }
+
+    return {};
   },
 });

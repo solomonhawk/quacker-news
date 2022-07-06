56d7bc62112a50c884f81778f70022f5430f583a
Solomon Hawk <solomon.hawk@viget.com>
2022-07-05 20:02:58 -0400

    fix prisma "too many clients" error
    - due to hot reload, we must cache the global PrismaClient singleton to prevent extra instantiation

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,21 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
 
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+
+  prisma = global.prisma;
+}
+
+export { prisma };

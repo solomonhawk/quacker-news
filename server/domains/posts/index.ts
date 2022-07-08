cee02ab2fa80d0ef3ffc5926b72863fd5530996f
Solomon Hawk <solomon.hawk@viget.com>
2022-07-08 03:54:21 -0400

    extract common post querying code

--- server/domains/posts/index.ts
+++ server/domains/posts/index.ts
@@ -1,41 +1,44 @@
-import { PostType } from '@prisma/client';
+import { PostType, Prisma, PrismaClient } from '@prisma/client';
 import { AuthedContext, Context, Requestless } from 'server/context';
 import { z } from 'zod';
 import { commentWithAuthorAndUserUpvote } from '../comments';
 import { threadComments } from '../comments/helpers';
 import { createPostSchema, derivePostType } from './helpers';
 
-export async function all(ctx: Requestless<Context>, page: number, perPage: number, type?: PostType) {
-  const skip = (page - 1) * perPage;
+type Pagination = {
+  page: number;
+  skip: number;
+  perPage: number;
+};
 
-  const [totalCount, posts] = await ctx.prisma.$transaction([
-    ctx.prisma.post.count({
-      where: {
-        type,
-        hiddenPosts: {
-          none: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-      },
-    }),
-    ctx.prisma.post.findMany({
+type PaginatedPostsQueryParams = {
+  prisma: PrismaClient;
+  pagination: Pagination;
+  where?: Prisma.PostWhereInput;
+  orderBy?: Prisma.PostOrderByWithRelationInput;
+  userId?: string;
+};
+
+const queryPaginatedPosts = async ({
+  prisma,
+  pagination: { page, skip, perPage },
+  where,
+  orderBy,
+  userId,
+}: PaginatedPostsQueryParams) => {
+  const [totalCount, posts] = await prisma.$transaction([
+    prisma.post.count({ where }),
+    prisma.post.findMany({
       skip,
       take: perPage,
-      where: {
-        type,
-        hiddenPosts: {
-          none: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-      },
+      where,
+      orderBy,
       include: {
         _count: true,
         upvotes: {
-          take: ctx.session?.user?.id ? 1 : 0,
+          take: userId ? 1 : 0,
           where: {
-            userId: ctx.session?.user?.id,
+            userId,
           },
           select: {
             id: true,
@@ -48,15 +51,21 @@ export async function all(ctx: Requestless<Context>, page: number, perPage: numb
           },
         },
         favorites: {
-          take: ctx.session?.user?.id ? 1 : 0,
+          take: userId ? 1 : 0,
           where: {
-            userId: ctx.session?.user?.id,
+            userId,
+          },
+          select: {
+            id: true,
           },
         },
         hiddenPosts: {
-          take: ctx.session?.user?.id ? 1 : 0,
+          take: userId ? 1 : 0,
           where: {
-            userId: ctx.session?.user?.id,
+            userId,
+          },
+          select: {
+            id: true,
           },
         },
       },
@@ -71,13 +80,38 @@ export async function all(ctx: Requestless<Context>, page: number, perPage: numb
     posts: posts.map((post, i) => {
       return {
         ...post,
-        favorited: ctx.session?.user?.id ? post.favorites.length > 0 : false,
-        hidden: ctx.session?.user?.id ? post.hiddenPosts.length > 0 : false,
-        upvoted: ctx.session?.user?.id ? post.upvotes.length > 0 : false,
+        favorited: userId ? post.favorites.length > 0 : false,
+        hidden: userId ? post.hiddenPosts.length > 0 : false,
+        upvoted: userId ? post.upvotes.length > 0 : false,
         position: skip + i + 1,
       };
     }),
   };
+};
+
+export async function all(ctx: Requestless<Context>, page: number, perPage: number, type?: PostType) {
+  const pagination = {
+    page,
+    skip: (page - 1) * perPage,
+    perPage,
+  };
+
+  const where = {
+    type,
+    hiddenPosts: {
+      none: {
+        userId: ctx.session?.user?.id,
+      },
+    },
+  };
+
+  const orderBy = {
+    upvotes: {
+      _count: 'desc' as const,
+    },
+  };
+
+  return queryPaginatedPosts({ prisma: ctx.prisma, pagination, where, orderBy, userId: ctx.session?.user.id });
 }
 
 export async function byId(ctx: Requestless<Context>, id: string) {
@@ -139,149 +173,39 @@ export async function byId(ctx: Requestless<Context>, id: string) {
 }
 
 export async function hidden(ctx: Requestless<Context>, page: number, perPage: number) {
-  const skip = (page - 1) * perPage;
-
-  const [totalCount, posts] = await ctx.prisma.$transaction([
-    ctx.prisma.post.count({
-      where: {
-        hiddenPosts: {
-          some: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-      },
-    }),
-    ctx.prisma.post.findMany({
-      skip,
-      take: perPage,
-      where: {
-        hiddenPosts: {
-          some: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-      },
-      include: {
-        _count: true,
-        upvotes: {
-          take: ctx.session?.user?.id ? 1 : 0,
-          where: {
-            userId: ctx.session?.user?.id,
-          },
-          select: {
-            id: true,
-          },
-        },
-        author: {
-          select: {
-            id: true,
-            username: true,
-          },
-        },
-        favorites: {
-          take: ctx.session?.user?.id ? 1 : 0,
-          where: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-        hiddenPosts: {
-          take: ctx.session?.user?.id ? 1 : 0,
-          where: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-      },
-    }),
-  ]);
-
-  return {
+  const pagination = {
     page,
+    skip: (page - 1) * perPage,
     perPage,
-    totalCount,
-    totalPages: Math.ceil(totalCount / perPage),
-    posts: posts.map((post, i) => {
-      return {
-        ...post,
-        favorited: ctx.session?.user?.id ? post.favorites.length > 0 : false,
-        hidden: ctx.session?.user?.id ? post.hiddenPosts.length > 0 : false,
-        upvoted: ctx.session?.user?.id ? post.upvotes.length > 0 : false,
-        position: skip + i + 1,
-      };
-    }),
   };
+
+  const where = {
+    hiddenPosts: {
+      some: {
+        userId: ctx.session?.user?.id,
+      },
+    },
+  };
+
+  return queryPaginatedPosts({ prisma: ctx.prisma, pagination, where, userId: ctx.session?.user.id });
 }
 
 export async function favorites(ctx: Requestless<Context>, page: number, perPage: number) {
-  const skip = (page - 1) * perPage;
-
-  const [totalCount, posts] = await ctx.prisma.$transaction([
-    ctx.prisma.post.count({
-      where: {
-        favorites: {
-          some: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-      },
-    }),
-    ctx.prisma.post.findMany({
-      skip,
-      take: perPage,
-      where: {
-        favorites: {
-          some: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-      },
-      include: {
-        _count: true,
-        upvotes: {
-          take: ctx.session?.user?.id ? 1 : 0,
-          where: {
-            userId: ctx.session?.user?.id,
-          },
-          select: {
-            id: true,
-          },
-        },
-        author: {
-          select: {
-            id: true,
-            username: true,
-          },
-        },
-        favorites: {
-          take: ctx.session?.user?.id ? 1 : 0,
-          where: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-        hiddenPosts: {
-          take: ctx.session?.user?.id ? 1 : 0,
-          where: {
-            userId: ctx.session?.user?.id,
-          },
-        },
-      },
-    }),
-  ]);
-
-  return {
+  const pagination = {
     page,
+    skip: (page - 1) * perPage,
     perPage,
-    totalCount,
-    totalPages: Math.ceil(totalCount / perPage),
-    posts: posts.map((post, i) => {
-      return {
-        ...post,
-        favorited: ctx.session?.user?.id ? post.favorites.length > 0 : false,
-        hidden: ctx.session?.user?.id ? post.hiddenPosts.length > 0 : false,
-        upvoted: ctx.session?.user?.id ? post.upvotes.length > 0 : false,
-        position: skip + i + 1,
-      };
-    }),
   };
+
+  const where = {
+    favorites: {
+      some: {
+        userId: ctx.session?.user?.id,
+      },
+    },
+  };
+
+  return queryPaginatedPosts({ prisma: ctx.prisma, pagination, where, userId: ctx.session?.user.id });
 }
 
 export const create = async (ctx: Requestless<AuthedContext>, input: z.infer<typeof createPostSchema>) => {
